e4d2b2a2c2637e3d6186cd602dc3822479153558
sssilver <sssilver@gmail.com>
2014-09-05 03:54:46 -0500

    Game struct stub. The game will now run infinitely and quit upon pressing Escape.

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,57 @@ use ncurses::*;
 
 fn main()
 {
-    /* Start ncurses. */
+    // Start ncurses
     initscr();
 
-    /* Print to the back buffer. */
+    // Print to the back buffer
     printw("Hello, world!");
 
-    /* Update the screen. */
+    // Update the screen
     refresh();
 
-    /* Wait for a key press. */
-    getch();
+    let mut game = Game {  // Make the game mutable
+        is_running: true
+    };
 
-    /* Terminate ncurses. */
+    game.initialize();
+    
+    while game.is_running() {
+        game.run();
+
+        // Read a key press
+        let key = getch();
+        if key == 27 {  // ESC pressed; quit the game
+            game.quit();
+        }
+
+        printw(format!("{:d} pressed", key).as_slice());
+    }
+
+    // Terminate ncurses
     endwin();
+}
+
+
+struct Game {
+    is_running: bool
+}
+
+
+impl Game {
+    fn initialize(&self) {
+        printw("Initializing the game");
+    }
+    
+    fn run(&self) {
+        printw("Running the game");
+    }
+
+    fn is_running(&self) -> bool {
+        return self.is_running;
+    }
+
+    fn quit(&mut self) {
+        self.is_running = false;
+    }
 }
